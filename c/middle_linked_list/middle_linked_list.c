f4f3629b5639fda3126fab5800ae38420646ea4e
isarwar93 <ismailsarwar7@gmail.com>
2023-02-25 21:14:36 +0100

    basic linked lists traversing and middle node find

--- c/middle_linked_list/middle_linked_list.c
+++ c/middle_linked_list/middle_linked_list.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 /* Given the head of a singly linked list, return the middle node of the linked list.
 
 If there are two middle nodes, return the second middle node.
@@ -30,11 +31,93 @@ The number of nodes in the list is in the range [1, 100].
  *     struct ListNode *next;
  * };
  */
+ struct ListNode 
+ {
+    int val;
+    struct ListNode *next;
+ };
 struct ListNode* middleNode(struct ListNode* head)
 {
-
+    int i=0;
+    struct ListNode * temp;
+    temp = head;
+    while(temp != NULL)
+    {
+        i++;
+        temp = temp -> next;
+    }
+    temp = head;
+    int j=1;
+    if (i%2==0)
+    {
+        while(j != ((i/2)+1))
+        {   
+            j++;
+            temp = temp -> next;
+        }
+        return temp;
+    }
+    else
+    {   
+        while(j != ((i+1)/2))
+        {
+            temp = temp -> next;
+            j++;
+        }
+        return temp;
+    }
+}
+print_linked_list(struct ListNode* x)
+{
+    struct ListNode* temp = x;
+    printf("[");
+    while(temp != NULL)
+    {
+        printf("%d, ",temp->val);
+        temp = temp -> next;
+    }  
+    printf("]");  
 }
 int main()
 {
+    struct ListNode* head = NULL;
+    struct ListNode* second = NULL;
+    struct ListNode* third = NULL;
+    struct ListNode* forth = NULL;
+    struct ListNode* fifth = NULL;
+    struct ListNode* sixth = NULL;
+    struct ListNode* seventh = NULL;
 
+    // allocate 3 nodes in the heap
+    head = (struct ListNode*)malloc(sizeof(struct ListNode*));
+    second = (struct ListNode*)malloc(sizeof(struct ListNode*));
+    third = (struct ListNode*)malloc(sizeof(struct ListNode*));
+    forth = (struct ListNode*)malloc(sizeof(struct ListNode*));
+    fifth = (struct ListNode*)malloc(sizeof(struct ListNode*));
+    sixth = (struct ListNode*)malloc(sizeof(struct ListNode*));
+    seventh = (struct ListNode*)malloc(sizeof(struct ListNode*));
+  
+    head->val = 1; // assign data in first node
+    head->next = second; // Link first node with second
+  
+    second->val = 2; // assign data to second node
+    second->next = third;
+
+    third->val = 3; // assign data in first node
+    third->next = forth; // Link first node with second
+  
+    forth->val = 4; // assign data to second node
+    forth->next = fifth;
+
+    fifth->val = 5; // assign data in first node
+    fifth->next = sixth; // Link first node with second
+  
+    sixth->val = 6; // assign data to second node
+    sixth->next = seventh;
+  
+    seventh->val = 7; // assign data to third node
+    seventh->next = NULL;
+
+    print_linked_list(head);
+    
 }
